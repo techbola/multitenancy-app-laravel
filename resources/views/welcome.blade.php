1ec2be1f487a3b88e210014851d645a9359ef45e
Adebola Fagbowore <techboladev@Adebolas-MacBook-Pro.local>
2021-03-20 02:34:15 -0700

    tenant global scope

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -39,6 +39,10 @@
                         {{ config('app.name') }}
                     </h1>
 
+                    <div>
+                        {{ App\Models\User::count() }}
+                    </div>
+
                     <ul class="list-reset">
                         <li class="inline px-4">
                             <a href="https://tailwindcss.com" class="font-medium text-indigo-600 hover:text-indigo-500 focus:outline-none focus:underline transition ease-in-out duration-150">Tailwind CSS</a>
